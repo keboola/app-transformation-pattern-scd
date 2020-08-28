32012ba2168c3de917224f52657976a4f7016c54
Ondra Jodas <ondrej.jodas@keboola.com>
2020-08-28 12:48:09 +0200

    basic sql generate test

--- tests/functional/DatadirTest.php
+++ tests/functional/DatadirTest.php
@@ -2,10 +2,21 @@
 
 declare(strict_types=1);
 
-namespace MyComponent\FunctionalTests;
+namespace Keboola\TransformationPatternScd\FunctionalTests;
 
 use Keboola\DatadirTests\DatadirTestCase;
+use Keboola\DatadirTests\DatadirTestSpecificationInterface;
+use Keboola\DatadirTests\DatadirTestsProviderInterface;
 
 class DatadirTest extends DatadirTestCase
 {
+    /**
+     * @return DatadirTestsProviderInterface[]
+     */
+    protected function getDataProviders(): array
+    {
+        return [
+            new DatadirTestProvider($this->getTestFileDir()),
+        ];
+    }
 }
