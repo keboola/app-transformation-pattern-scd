629d784e654983efef94394de25c88fa7b7c1653
Ondřej Jodas <ondra.jodas@gmail.com>
2025-10-07 22:47:57 +0200

    Refactor InputTableResolverTest to use dataProviders

--- tests/phpunit/InputTableResolverTest.php
+++ tests/phpunit/InputTableResolverTest.php
@@ -27,55 +27,6 @@ class InputTableResolverTest extends TestCase
         $this->resolver = new InputTableResolver($this->config, $this->apiFacade);
     }
 
-    public function testGetInputTableIdWithSingleTable(): void
-    {
-        $this->config->expects($this->once())->method('getInputTables')->willReturn([
-            ['source' => 'in.c-main.test_table'],
-        ]);
-
-        $result = $this->resolver->getInputTableId();
-
-        self::assertEquals('in.c-main.test_table', $result);
-    }
-
-    public function testGetInputTableIdWithMultipleTables(): void
-    {
-        $this->config->expects($this->once())->method('getInputTables')->willReturn([
-            ['source' => 'in.c-main.table1', 'destination' => 'other_table'],
-            ['source' => 'in.c-main.table2', 'destination' => 'input_table'],
-            ['source' => 'in.c-main.table3', 'destination' => 'another_table'],
-        ]);
-
-        $result = $this->resolver->getInputTableId();
-
-        self::assertEquals('in.c-main.table2', $result);
-    }
-
-    public function testGetInputTableIdWithEmptyTables(): void
-    {
-        $this->config->expects($this->once())->method('getInputTables')->willReturn([]);
-
-        $this->expectException(UserException::class);
-        $this->expectExceptionMessage('Please specify one input table in the input mapping.');
-
-        $this->resolver->getInputTableId();
-    }
-
-    public function testGetInputTableIdWithMultipleTablesNoInputTable(): void
-    {
-        $this->config->expects($this->once())->method('getInputTables')->willReturn([
-            ['source' => 'in.c-main.table1', 'destination' => 'other_table'],
-            ['source' => 'in.c-main.table2', 'destination' => 'another_table'],
-        ]);
-
-        $this->expectException(UserException::class);
-        $this->expectExceptionMessage(
-            'Found "2" tables in input mapping, but no source table with "destination" = "input_table". ' .
-            'Please set the source table in the input mapping.'
-        );
-
-        $this->resolver->getInputTableId();
-    }
 
     public function testGetInputTableColumns(): void
     {
@@ -89,38 +40,17 @@ class InputTableResolverTest extends TestCase
         self::assertEquals(['id', 'name', 'email', 'phone'], $result);
     }
 
-    public function testGetInputTableDefinition(): void
+    /**
+     * @dataProvider tableDefinitionProvider
+     */
+    public function testGetInputTableDefinition(array $tableData, array $expectedDefinition): void
     {
         $this->setupConfigWithSingleTable();
-        $this->apiFacade->expects($this->once())->method('getTable')->willReturn([
-            'definition' => [
-                'columns' => [
-                    ['name' => 'id', 'definition' => ['type' => 'VARCHAR']],
-                    ['name' => 'name', 'definition' => ['type' => 'VARCHAR']],
-                ],
-            ],
-        ]);
+        $this->apiFacade->expects($this->once())->method('getTable')->willReturn($tableData);
 
         $result = $this->resolver->getInputTableDefinition();
 
-        self::assertEquals([
-            'columns' => [
-                ['name' => 'id', 'definition' => ['type' => 'VARCHAR']],
-                ['name' => 'name', 'definition' => ['type' => 'VARCHAR']],
-            ],
-        ], $result);
-    }
-
-    public function testGetInputTableDefinitionWithoutDefinition(): void
-    {
-        $this->setupConfigWithSingleTable();
-        $this->apiFacade->expects($this->once())->method('getTable')->willReturn([
-            'columns' => ['id', 'name'],
-        ]);
-
-        $result = $this->resolver->getInputTableDefinition();
-
-        self::assertEquals([], $result);
+        self::assertEquals($expectedDefinition, $result);
     }
 
     public function testCachingOfTableDetail(): void
@@ -202,6 +132,33 @@ class InputTableResolverTest extends TestCase
             ],
             'expectedTableId' => 'in.c-main.table2',
             'shouldThrowException' => false,
+        ];
+    }
+
+    public function tableDefinitionProvider(): Generator
+    {
+        yield 'with definition' => [
+            'tableData' => [
+                'definition' => [
+                    'columns' => [
+                        ['name' => 'id', 'definition' => ['type' => 'VARCHAR']],
+                        ['name' => 'name', 'definition' => ['type' => 'VARCHAR']],
+                    ],
+                ],
+            ],
+            'expectedDefinition' => [
+                'columns' => [
+                    ['name' => 'id', 'definition' => ['type' => 'VARCHAR']],
+                    ['name' => 'name', 'definition' => ['type' => 'VARCHAR']],
+                ],
+            ],
+        ];
+
+        yield 'without definition' => [
+            'tableData' => [
+                'columns' => ['id', 'name'],
+            ],
+            'expectedDefinition' => [],
         ];
     }
 
